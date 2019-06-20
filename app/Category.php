cc7f4630f45c82960f21656c9c9bfd917444687e
Maki Fukudome <mk1282130@gmail.com>
2019-06-20 13:46:18 +0900

    admin create new category

---
+++ app/Category.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    // protected $table = 'category';
+    // モデルは自動的に複数形でデータベースネームを探そうとする
+    // protectedを加えることで、ここでは単数形の名前で指定させている
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+}
